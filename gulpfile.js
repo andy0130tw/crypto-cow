df55a97b5da47bc9103863940aa4ede56bab6ce5
Andy Pan <pan@pelith.com>
2019-02-05 19:11:49 +0000

    split vendor/app bundle at debug

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,56 @@ const fs = require('fs');
 const path = require('path');
 
 const gulp = require('gulp');
-const tfilter = require('tfilter');
+// const tfilter = require('tfilter');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const browserify = require('browserify');
 const watchify = require('watchify');
 const babelify = require('babelify');
 const chalk = require('chalk');
-const exorcist = require('exorcist')
+const exorcist = require('exorcist');
+const stream = require('stream');
+
+const uglify = require('gulp-uglify');
 
 let appRootPath = path.join(__dirname);
+
+const externalDeps = [
+  'web3',
+  'bn.js',
+  'pixi.js',
+  '@material/drawer',
+  '@material/ripple',
+  '@material/dialog',
+  '@material/textfield',
+  '@material/snackbar',
+  '@material/notched-outline',
+  '@material/list',
+];
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const externalsTransform = (() => {
+  const extPath = 'dist/vendor.js'
+  let transformed = false
+
+  function transform(buf, enc, next) {
+    if (!transformed) {
+      this.push(`(function(d){t=d.createElement('script');t.src='${extPath}';d.head.appendChild(t);t.onload=(function(){`);
+      transformed = true
+    }
+
+    this.push(buf);
+    next();
+  }
+
+  function flush() {
+    this.push(`})})(document)`)
+    this.push(null)
+  }
+
+  return new stream.Transform({ transform, flush })
+})()
 
 function getBrowserifyBase(debug) {
   let plugin = [];
@@ -22,16 +62,12 @@ function getBrowserifyBase(debug) {
   let config = Object.assign({
     entries: 'src/js/script.js',
     plugin: plugin,
-    debug: debug
+    debug: debug,
   }, watchify.args);
 
   return browserify(config)
-    .transform(babelify)
-    .transform(tfilter(babelify, {
-      include: path.join(appRootPath, '/node_modules/@material/**/*.js')
-    }), {
-      global: true
-    });
+    .external(debug ? externalDeps : [])
+    .transform(babelify);
 }
 
 function bundle(b) {
@@ -48,8 +84,30 @@ function bundle(b) {
     .pipe(gulp.dest('./src/dist'));
 }
 
+gulp.task('vendor', () => {
+  let b = browserify({
+    require: externalDeps,
+    debug: true,
+  });
+
+  b.on('package', function (pkg) {
+    console.log(pkg.name);
+  });
+
+  return b
+    .bundle()
+    .pipe(source('vendor.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./src/dist'));
+});
+
 gulp.task('debug', () => {
   let b = getBrowserifyBase(true);
+
+  b.on('bundle', () => {
+    b.pipeline.get('wrap').unshift(externalsTransform);
+  });
+
   b.on('log', log => {
     console.log(`[${chalk.green('watchify')}] ${chalk.bold('Build success')}: ${chalk.green(log)}`);
   });
@@ -62,7 +120,7 @@ gulp.task('build', () => {
   return b.bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
-    .pipe(require('gulp-uglify')())
+    .pipe(uglify())
     .pipe(gulp.dest('./src/dist'));
 
   return bundle(b);
